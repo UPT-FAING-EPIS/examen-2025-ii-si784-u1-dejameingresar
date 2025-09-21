c81c10416489c0ef972e389ff229ec6979f74bc3
dejamepushear <143133116+dejameingresar@users.noreply.github.com>
2025-09-20 20:23:33 -0500

    Delete frontend directory

--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-# Frontend placeholder
-You can create a React (Vite) or Angular app here. Example:
-npx create-vite@latest web-react --template react
--- frontend/src/App.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export default function App(){return <div style={{padding:20}}><h1>Tienda - Frontend</h1><p>Conecta al backend en /api/</p></div>}
